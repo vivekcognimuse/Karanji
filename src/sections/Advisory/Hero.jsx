e5c17e5f635eaedd1c9e8ec5dd7418b9ec1b6d72
Sandeshbhat234 <101869011+Sandeshbhat234@users.noreply.github.com>
2025-08-25 19:51:13 +0530

    feat:add logo animation for company page

--- src/sections/Advisory/Hero.jsx
+++ src/sections/Advisory/Hero.jsx
@@ -5,6 +5,7 @@ import { P1 } from "@/components/CustomTags";
 // client-only, returns null, runs GSAP
 import HeroReveal from "@/components/animations/HeroReveal";
 import ScrollButton from "@/components/ScrollButton";
+import LogoAnimation from "@/components/LogoAnimation";
 
 export default function HeroSection({ data, bgImage }) {
   const { title, ctaText, ctaLink, subTitle, stats, backgroundImage } = data;
@@ -16,22 +17,20 @@ export default function HeroSection({ data, bgImage }) {
       className={`relative bg-no-repeat flex flex-col h-fit pt-16 md:pt-0 lg:min-h-[calc(100vh-80px)] bg-top bg-contain`}
       style={{
         backgroundImage: `url('${bgImage}')`,
-      }}
-    >
+      }}>
       <div className="space-y-6 sm:space-y-8 lg:flex-grow flex flex-col justify-center  max-w-[1580px] lg:mx-auto">
+        <LogoAnimation />
         <div className="space-y-3 sm:space-y-4">
           <h2
             className="text-center opacity-0 will-change-transform"
-            data-reveal
-          >
+            data-reveal>
             {title}
           </h2>
 
           {subTitle && (
             <P1
               className="text-black text-center mx-auto opacity-0 will-change-transform"
-              data-reveal
-            >
+              data-reveal>
               {subTitle}
             </P1>
           )}
@@ -53,8 +52,7 @@ export default function HeroSection({ data, bgImage }) {
                 <div
                   key={index}
                   className={`px-4 sm:px-6 flex border-l border-black/30 gap-3 sm:gap-4 items-start lg:items-center justify-start text-left${addLgBorderLeft} opacity-0 will-change-transform`}
-                  data-reveal
-                >
+                  data-reveal>
                   <div className="text-lg lg:text-[2.62rem] font-semibold font-sans text-black sm:mx-0">
                     {card.number}
                   </div>
