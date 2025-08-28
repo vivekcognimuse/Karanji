e3be1c1ce9f5594b36a573485de38e738042a7d9
vivekcognimuse <vivekbhat@cognimuse.com>
2025-08-28 15:56:58 +0530

    fix: sidepadding for upcoming component in resource page

--- src/app/company/careers/page.js
+++ src/app/company/careers/page.js
@@ -57,17 +57,17 @@ const ctaData = {
 
 export default function careersPage() {
   return (
-    <main className="w-full max-w-[1580px] mx-auto px-4 lg:px-10 space-y-16 lg:space-y-32">
+    <main className="w-full max-w-[1580px] mx-auto px-4 sm:px-6 lg:px-10 space-y-16 lg:space-y-32">
       {/* Wrapper for all components except the last CTA */}
       <div
-        className="p-10 mb-30 my-auto rounded-[32px]"
+        className="px-6 py-8 sm:px-8 sm:py-10 lg:px-12 lg:py-12 mb-30 my-auto rounded-[32px]"
         style={{
           background:
             "linear-gradient(93.27deg, rgba(158, 135, 255, 0.1) 8.1%, rgba(109, 191, 254, 0.1) 41.6%, rgba(255, 143, 143, 0.1) 95.33%, rgba(255, 255, 255, 0.1) 127.34%)",
           backdropFilter: "blur(28.100000381469727px)",
           boxShadow: "0px 4px 4px 0px rgba(204, 204, 204, 0.25)",
         }}>
-        <div className="space-y-16 lg:space-y-32 ">
+        <div className="space-y-16 lg:space-y-32">
           <CTA
             data={teamData}
             className="
