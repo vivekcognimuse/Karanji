30e9222b4d4092fb551eff86d7cf970a4416ad18
vivekcognimuse <vivekbhat@cognimuse.com>
2025-09-09 13:27:08 +0530

    fix: added form validation in register webinar form

--- src/lib/strapi.js
+++ src/lib/strapi.js
@@ -12,7 +12,7 @@ export async function fetchFromStrapi(endpoint, options = {}, baseUrl) {
 
   const {
     populate = "all", // Default populate
-    revalidate = 3600, // No revalidate by default (pure SSG)
+    revalidate = 30, // No revalidate by default (pure SSG)
   } = options;
 
   const url = new URL(`${baseUrl}/${endpoint}`);
