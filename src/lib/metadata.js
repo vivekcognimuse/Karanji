be45fb2b1f61bd8a89bbdb0a834bdea5831eec79
Sandeshbhat234 <101869011+Sandeshbhat234@users.noreply.github.com>
2025-09-15 10:45:17 +0530

    remove unwanted things in industry page

--- src/lib/metadata.js
+++ src/lib/metadata.js
@@ -6,8 +6,12 @@ export async function getMetadata(endpoint) {
     const { title, description } = data?.metaData || {};
 
     return {
-      title: title,
-      description: description,
+      title:
+        title ||
+        "Karanji - Transforming Learning with AI, XR, and Digital Twins",
+      description:
+        description ||
+        "Explore how Karanji is revolutionizing industries with cutting-edge technology.",
     };
   } catch (error) {
     console.error(`Error fetching metadata for ${endpoint}:`, error);
